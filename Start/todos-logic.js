4119855f8323a31f063a7d8f1385820e3f602f77
Risto Ikonen <ristoikonen@gmail.com>
2024-08-05 20:25:37 +1000

    Work with..

--- Start/todos-logic.js
+++ Start/todos-logic.js
@@ -1,7 +1,7 @@
 const markTodoAsCompleted = (id) => {
     $.ajax({
         type: 'PUT',
-        url: `http://localhost:8000/todos/${id}/completed`,
+        url: 'http://localhost:8000/todos/${id}/completed',
         success: function(todos) {
             console.log(todos);
             setTodos(todos);
@@ -9,15 +9,48 @@ const markTodoAsCompleted = (id) => {
     });
 }
 
+function getForecast() {
+    fetch('http://localhost:8000/todos', {
+        mode:  'no-cors' ,
+        method: 'GET',
+        headers: {
+          //'Content-Type': 'application/json'
+          'Content-Type': 'application/text'
+        },
+        // body: JSON.stringify({
+        //   key1: 'value1',
+        //   key2: 'value2'
+        // })
+      })
+      //.then(response => response.json())
+      .then(data => console.log(data))
+      .catch(error => console.error('Unable to get forecast.', error));
+}
+
 const createTodo = () => {
+    console.log('Create POST todo');
     const newTodoText = $('#new-todo-input').prop('value');
     $.ajax({
         type: 'POST',
-        url: `http://localhost:8000/todos`,
+        url: 'http://localhost:8000/todos',
         data: JSON.stringify({ text: newTodoText }),
         contentType: 'application/json',
         success: function(todos) {
-            setTodos(todos);
+            setPOSTTodos(todos);
+        },
+    });
+}
+
+const getTodo = async () => {
+    console.log('a GET todo');
+    const newTodoText = $('#new-todo-input').prop('value');
+    $.ajax({
+        type: 'GET',
+        url: 'http://localhost:8000/todos',
+        
+        contentType: 'application/json',
+        success: function(todos) {
+            setGETTodos(todos);
         },
     });
 }
@@ -25,7 +58,7 @@ const createTodo = () => {
 const deleteTodo = (id) => {
     $.ajax({
         type: 'DELETE',
-        url: `http://localhost:8000/todos/${id}`,
+        url: 'http://localhost:8000/todos/${id}',
         success: function(todos) {
             setTodos(todos);
         },
@@ -50,14 +83,28 @@ const addTodo = (todo) => {
 
 const setTodos = (todos) => {
     $('#todos-list').empty();
-    todos.forEach(todo => addTodo(todo));
+    $('#todos-list').append('<h2>Todos</h2>');
+    //todos.forEach(todo => addTodo(todo));
 }
 
-$(() => {
-    $.ajax({
-        url: 'http://localhost:8000/todos',
-        success: function(todos) {
-            setTodos(todos);
-        },
-    });
-});
+const setPOSTTodos = (todos) => {
+    $('#todos-list').empty();
+    $('#todos-list').append('<h2>POST Todos</h2>' + '<h3>' + todos.text + '</h3>');
+    //todos.forEach(todo => addTodo(todo));
+}
+
+const setGETTodos = (todos) => {
+    $('#todos-list').empty();
+    $('#todos-list').append('<h2>GEt Todos</h2>' + '<h3>' + todos.text + '</h3>');
+    //todos.forEach(todo => addTodo(todo));
+}
+
+
+// $(() => {
+//     $.ajax({
+//         url: 'http://localhost:8000/todos',
+//         success: function(todos) {
+//             setTodos(todos);
+//         },
+//     });
+// });
